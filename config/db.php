38c7555be9e19333961570b5996ac417e16e9299
ngigi erick <shngigierick@gmail.com>
2025-03-05 14:55:26 +0300

    change the db config to podtgres and started on the modules

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'pgsql:host=localhost;dbname=ai_resume_db', // Change 'ai_resume_db' to your actual database name
-    'username' => 'your_pg_username', // Replace with your PostgreSQL username
-    'password' => 'your_pg_password', // Replace with your PostgreSQL password
+    'username' => '"postgres"', // Replace with your PostgreSQL username
+    'password' => '123456', // Replace with your PostgreSQL password
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
--- config/web.php
+++ config/web.php
@@ -1,37 +1,35 @@
 <?php
 
+// Load application parameters and database configuration
 $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
 
 $config = [
-    'id' => 'basic',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
+    'id' => 'basic', // Application ID
+    'basePath' => dirname(__DIR__), // Base directory of the application
+    'bootstrap' => ['log'], // Components to be loaded at the start
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
     'components' => [
         'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            // Security: Cookie validation key (change this for production)
             'cookieValidationKey' => 'VECBL0beyObq5u5pPyxsWNaH8qPPWfQF',
         ],
         'cache' => [
-            'class' => 'yii\caching\FileCache',
+            'class' => 'yii\caching\FileCache', // Enables file caching
         ],
         'user' => [
-            'identityClass' => 'app\models\User',
-            'enableAutoLogin' => true,
+            'identityClass' => 'app\models\User', // User authentication class
+            'enableAutoLogin' => true, // Enables "remember me" functionality
         ],
         'errorHandler' => [
-            'errorAction' => 'site/error',
+            'errorAction' => 'site/error', // Custom error handling route
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'useFileTransport' => true, // Set to false for real email sending
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
@@ -42,33 +40,42 @@ $config = [
                 ],
             ],
         ],
-        'db' => $db,
-        /*
+        'db' => $db, // Database connection (PostgreSQL in our case)
+
         'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
+            'enablePrettyUrl' => true, // Enables SEO-friendly URLs
+            'showScriptName' => false, // Hides index.php in URLs
             'rules' => [
+                // Define custom URL rules here
             ],
         ],
-        */
     ],
-    'params' => $params,
+    'modules' => [
+        'user' => [
+            'class' => 'app\modules\user\Module',
+        ],
+        'resume' => [
+            'class' => 'app\modules\resume\Module',
+        ],
+        'payment' => [
+            'class' => 'app\modules\payment\Module',
+        ],
+    ],
+    'params' => $params, // Application parameters
 ];
 
 if (YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
+    // Configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+        //'allowedIPs' => ['127.0.0.1', '::1'], // Restrict debug access
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+        //'allowedIPs' => ['127.0.0.1', '::1'], // Restrict Gii access
     ];
 }
 
